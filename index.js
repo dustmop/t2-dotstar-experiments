31e4cd5862f093ff9514f4f9237b54833f26dc46
Dustin Long <me@dustmop.io>
2016-09-04 18:52:51 -0400

    Flex sensor controls the led strip

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const tessel = require('tessel')
 const color = require('color')
 const DotStarStrip = require('./dotstar')
 
-let strip = new DotStarStrip(60, 'A')
+const numPixels = 60;
 
-let sensorValue = 0;
+let strip = new DotStarStrip(numPixels, 'A')
+
+let flexValue = 0;
 
 var sensor = tessel.port.B.pin[2];
-sensor.on('change', function(value) {
-  console.log('Sensor value is ', value);
-  sensorValue = value;
-});
+
+function scaleFlexSensor(rawValue) {
+  const rawMax = 3.3;
+  const rawMin = 1.764404296875;
+  const rawHeight = rawMax - rawMin;
+  const scaleRange = numPixels;
+  const scaleEffect = scaleRange / rawHeight;
+  return scaleRange - (rawValue - rawMin) * scaleEffect + 1;
+}
+
+setInterval(function() {
+  sensor.analogRead(function(err, rawValue) {
+    if (err) {
+      return;
+    }
+    flexValue = scaleFlexSensor(rawValue);
+  });
+}, 100);
 
 strip.init(4000000)
 
@@ -18,31 +34,34 @@ let options = {
   color: [0, 200, 255]
 }
 
-let i = 0;
+let cycle = 0;
 let r = g = b = 0;
 let n = 0;
 let choice = 0;
 
-let max = 60;
-
 console.log('starting...')
 
+setTimeout(function() {
 strip.clear(function() {
   setInterval(function() {
-    i++;
-    if (i * 1 >= max) {
-      i = 0;
+    cycle++;
+    if (cycle >= 2) {
+      cycle = 0;
       choice++;
       if (choice == 3) { choice = 0; }
     }
-    let limit = sensorValue;
-    for (let k = 0; k < limit; k++) {
-      r = choice == 0 ? 0xff : 0x00;
-      g = choice == 1 ? 0xff : 0x00;
-      b = choice == 2 ? 0xff : 0x00;
-      strip.poke({pixel: k, color: [r, g, b]})
+    let limit = flexValue;
+    for (let k = 0; k < numPixels; k++) {
+      if (k < limit) {
+        r = choice == 0 ? 0xff : 0x00;
+        g = choice == 1 ? 0xff : 0x00;
+        b = choice == 2 ? 0xff : 0x00;
+        strip.poke({pixel: k, color: [r, g, b]});
+      } else {
+        strip.poke({pixel: k, color: [0, 0, 0]})
+      }
     }
-    strip.poke({pixel: limit, color: [0, 0, 0]})
-    strip.send()
-  }, 20)
+    strip.send();
+  }, 200);
 });
+}, 50);
